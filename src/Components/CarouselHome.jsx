8742d3b18618866e8e8618130ff52e420a28c209
nikeshhub <nikeshsapkota2021@gmail.com>
2023-09-23 17:11:19 +0545

    dynamic carousel in homepage

--- src/Components/CarouselHome.jsx
+++ src/Components/CarouselHome.jsx
@@ -1,9 +1,14 @@
 import { Carousel } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CarouselHome = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [id, setId] = useState();
+  const navigate = useNavigate();
   const carouselSettings = {
     autoplay: true,
+    dots: false,
     centerMode: true,
     slidesToShow: 2, // Adjust the number of visible items
     responsive: [
@@ -16,40 +21,48 @@ const CarouselHome = () => {
     ],
   };
 
+  useEffect(() => {
+    // Fetch trending movies from TMDB API
+    fetch(
+      "https://api.themoviedb.org/3/trending/all/day?api_key=7075fe97f28375fc950eee9bfe2a0364"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the backdrop from the API response
+        const backdrop = data.results.map((result) => result.backdrop_path);
+        const id = data.results.map((result) => result.id);
+        setTrendingMovies(backdrop);
+        setId(id);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from TMDB API:", error);
+      });
+  }, []);
+  const handleCardClick = (movieId) => {
+    navigate(`/movies/${movieId}`);
+  };
+
   return (
     <div style={{ padding: "30px 0" }}>
       <Carousel {...carouselSettings}>
-        <div style={{ overflow: "hidden", borderRadius: "2.5%", marginRight:"10px"}}>
-          <img
-            src="./poster.jpg"
-            alt=""
-            style={{ width: "100%", height: "400px", objectFit: "cover" }}
-          />
-        </div>
-
-        <div style={{ overflow: "hidden", borderRadius: "2.5%" }}>
-          <img
-            src="./poster2.jpg"
-            alt=""
-            style={{ width: "100%", height: "400px", objectFit: "cover" }}
-          />
-        </div>
-
-        <div style={{ overflow: "hidden", borderRadius: "2.5%" }}>
-          <img
-            src="./poster3.jpg"
-            alt=""
-            style={{ width: "100%", height: "400px", objectFit: "cover" }}
-          />
-        </div>
-
-        <div style={{ overflow: "hidden", borderRadius: "2.5%" }}>
-          <img
-            src="./poster.jpg"
-            alt=""
-            style={{ width: "100%", height: "400px", objectFit: "cover" }}
-          />
-        </div>
+        {trendingMovies.map((backdrop, index) => (
+          <div key={index} style={{ overflow: "hidden", borderRadius: "2.5%" }}>
+            <img
+              onClick={() => {
+                handleCardClick(id);
+              }}
+              src={`https://image.tmdb.org/t/p/original${backdrop}`}
+              alt=""
+              style={{
+                width: "100%",
+                height: "400px",
+                objectFit: "cover",
+                objectPosition: "center center",
+                cursor: "pointer",
+              }}
+            />
+          </div>
+        ))}
       </Carousel>
     </div>
   );
