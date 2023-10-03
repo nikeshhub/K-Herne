aa084c729eca0820595237dbd440b0aa74445be4
Nikesh Sapkota <nikeshsapkota@Nikeshs-MacBook-Pro.local>
2023-10-03 20:21:37 +0545

    navigate solve

--- src/Components/TVSeriesPage.jsx
+++ src/Components/TVSeriesPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 
 const TVSeriesPage = () => {
+    const navigate = useNavigate()
   return (
     <div style={{ padding: '20px' }}>
       <h1>TV Series</h1>
       <p>Currently, TV series are not available.</p>
-      <Button type="primary" href="/movies">
+      <Button type="primary" onClick={()=>{navigate("/movies")}}>
         Browse Movies Instead
       </Button>
     </div>
